b1c687bb53423b31d659db62bb59ca3fe09421c9
Nikolai Makarevich <nikoladze529@mail.ru>
2022-06-22 18:15:17 +0300

    feat: add webSocketServer

--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
-import Jimp from 'jimp';
 import { WebSocketServer } from 'ws';
-import robot from 'robotjs';
 import httpServer from './src/http_server';
+import handleCommand from './src/commands/handleCommand';
 
 const HTTP_PORT = 3000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
+process.stdout.write(`Start static http server on the ${HTTP_PORT} port!\n`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    handleCommand(ws, data.toString());
+  });
+});
+
+process.on('SIGINT', () => {
+  process.stdout.write('Closing websocket...\n');
+  wss.close();
+  process.exit(0);
+});
