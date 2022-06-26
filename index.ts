b5d04ef1757a536d017f6d53122f4e43fc6a6f20
Nikolai Makarevich <nikoladze529@mail.ru>
2022-06-26 19:57:56 +0300

    refactor: add message on open websocket client

--- index.ts
+++ index.ts
@@ -4,12 +4,15 @@ import handleCommand from './src/commands/handleCommand';
 
 const HTTP_PORT = 3000;
 
+const WS_PORT = 8080;
+
 process.stdout.write(`Start static http server on http://localhost:${HTTP_PORT}!\n`);
 httpServer.listen(HTTP_PORT);
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: WS_PORT });
 
 wss.on('connection', (ws) => {
+  process.stdout.write(`Websocket is ran on port ${WS_PORT}\n`);
   const wsStream = createWebSocketStream(ws, { encoding: 'utf-8', decodeStrings: false });
   handleCommand(wsStream);
   ws.on('close', () => {
